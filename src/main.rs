2f4813e3bf9cb4d220cde2f1cc6b8fce008659fe
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-16 05:13:00 +0530

    ADD: enum for rescode

--- src/main.rs
+++ src/main.rs
@@ -173,6 +173,32 @@ impl BytePacketBuffer {
         }
         Ok(())
     }
+    
+}
+
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+// The enum defining the values of `rescode` field: Result of a DNS query. 
+pub enum ResultCode {
+    NOERROR = 0, 
+    FORMERR = 1, 
+    SERVFAIL = 2,
+    NXDOMAIN = 3, 
+    NOTIMP = 4, 
+    REFUSED = 5,
+}
+
+impl ResultCode {
+    pub fn from_num (num: u8) -> ResultCode {
+        match num {
+            1 => ResultCode::FORMERR,
+            2 => ResultCode::SERVFAIL,
+            3 => ResultCode::NXDOMAIN,
+            4 => ResultCode::NOTIMP,
+            5 => ResultCode::REFUSED,
+            0 | _ => ResultCode::NOERROR,
+        }
+    }
 }
 
 
