0792bf4b0cf4f8621e208d68dbc7733cde528b76
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-17 06:27:41 +0530

    ADD: completed/implementation of DNS packet

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 use std::fs::File;
 use std::io::Read;
 use std::net::Ipv4Addr;
-use std::result;
+use std::{result, vec};
 
 mod error;
 mod prelude;
@@ -369,7 +369,56 @@ impl DnsRecord {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct DnsPacket {
+    pub header: DnsHeader,
+    pub questions: Vec<DnsQuestion>,
+    pub answers: Vec<DnsRecord>,
+    pub authorities: Vec<DnsRecord>,
+    pub resources: Vec<DnsRecord>,
+}
 
+impl DnsPacket {
+    pub fn new() -> DnsPacket {
+        DnsPacket {
+            header: DnsHeader::new(),
+            questions: Vec::new(),
+            answers: DnsRecord::Vec::new(),
+            authorities: Vec::new(),
+            resources: Vec::new(),
+        }
+    }
+
+    pub fn from_buffer(buffer: &mut BytePacketBuffer) -> Result<DnsPacket> {
+        // parsing the header
+        let mut result = DnsPacket::new();
+        result.header.read(buffer)?;
+
+        // parsing the questions
+        for _ in 0..result.header.questions {
+            let mut question = DnsQuestion::new("".to_string(), QueryType::UNKNOWN(0));
+            question.read(buffer)?;
+            result.questions.push(question);
+        }
+
+        for _ in 0..result.header.answers {
+            let rec = DnsRecord::read(buffer)?;
+            result.answers.push(rec);
+        }
+
+        for _ in 0..result.header.authoritative_entries {
+            let rec = DnsRecord::read(buffer)?;
+            result.authorities.push(rec);
+        }
+
+        for _ in 0..result.header.resource_entries {
+            let rec = DnsRecord::read(buffer)?;
+            result.resources.push(rec);
+        }
+
+        Ok(result)
+    }
+}
 
 fn main() -> Result<()> {
     println!("This is the main function ");
