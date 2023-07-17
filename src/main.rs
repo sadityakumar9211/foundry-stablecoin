58aedc2ea13929f70bab0182282835ca646a19ae
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-17 06:15:18 +0530

    ADD: implementation of DNS record

--- src/main.rs
+++ src/main.rs
@@ -209,7 +209,7 @@ pub struct DnsHeader {
 
     pub rescode: ResultCode,       // 4 bits
     pub checking_disabled: bool,   // 1 bit
-    pub authed_data: bool,           // 1bit
+    pub authed_data: bool,         // 1bit
     pub z: bool,                   // 1 bit
     pub recursion_available: bool, // 1bit
 
@@ -268,9 +268,8 @@ impl DnsHeader {
     }
 }
 
-
 #[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
-pub enum QueryType{
+pub enum QueryType {
     UNKNOWN(u16),
     A, // 1
 }
@@ -280,36 +279,96 @@ impl QueryType {
     pub fn to_num(&self) -> u16 {
         match *self {
             QueryType::UNKNOWN(x) => x,
-            QueryType::A => 1, 
+            QueryType::A => 1,
         }
     }
 
-    pub fn from_num(num:u16) -> QueryType {
+    pub fn from_num(num: u16) -> QueryType {
         match num {
             1 => QueryType::A,
-            _ => QueryType::UNKNOWN(num)
+            _ => QueryType::UNKNOWN(num),
         }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct DnsQuestion {
-    pub name: String, 
-    pub qtype: QueryType, 
+    pub name: String,
+    pub qtype: QueryType,
 }
 
 impl DnsQuestion {
-    pub fn new (name: String, qtype: QueryType) -> DnsQuestion {
-        DnsQuestion { name: name, qtype: qtype }
+    pub fn new(name: String, qtype: QueryType) -> DnsQuestion {
+        DnsQuestion {
+            name: name,
+            qtype: qtype,
+        }
     }
 
     pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<()> {
         buffer.read_qname(&mut self.name)?;
         self.qtype = QueryType.from_num(buffer.read_u16()?); // qtype - Question Type
-        let _ = buffer.read_u16()?;   // class
+        let _ = buffer.read_u16()?; // class
         Ok(())
-    } 
+    }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[allow(dead_code)]
+pub enum DnsRecord {
+    UNKNOWN {
+        domain: String,
+        qtype: u16,
+        data_len: u16,
+        ttl: u32,
+    }, // 0
+    A {
+        domain: String,
+        addr: Ipv4Addr,
+        ttl: u32,
+    }, // 1
+}
+
+impl DnsRecord {
+    pub fn read(buffer: &mut BytePacketBuffer) -> Result<DnsRecord> {
+        let mut domain = String::new();
+        buffer.read_qname(&mut domain);
+
+        let qtype_num = buffer.read_u16()?;
+        let qtype = QueryType::from_num(qtype_num);
+        let _ = buffer.read_u16()?;
+        let ttl = buffer.read_u32()?;
+        let data_len = buffer.read_u16()?;
+
+        match qtype {
+            QueryType::A => {
+                let raw_addr = buffer.read_u32()?;
+                let addr = Ipv4Addr::new(
+                    ((raw_addr >> 24) & 0xFF) as u8,
+                    ((raw_addr >> 16) & 0xFF) as u8,
+                    ((raw_addr >> 8) & 0xFF) as u8,
+                    ((raw_addr >> 0) & 0xFF) as u8,
+                );
+
+                Ok(DnsRecord::A {
+                    domain: domain,
+                    addr: addr,
+                    ttl: ttl,
+                })
+            }
+            QueryType::UNKNOWN(_) => {
+                buffer.step(data_len as usize)?;
+                Ok(DnsRecord::UNKNOWN {
+                    domain: domain,
+                    qtype: qtype_num,
+                    data_len: data_len,
+                    ttl: ttl,
+                })
+            }
+        }
+    }
+}
+
 
 
 fn main() -> Result<()> {
