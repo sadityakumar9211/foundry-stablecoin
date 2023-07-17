53c62439f506cc8960c06fa7758760eeb0fdf290
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-17 05:56:06 +0530

    ADD: Added implementation for DNS Question field

--- src/main.rs
+++ src/main.rs
@@ -292,10 +292,27 @@ impl QueryType {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DnsQuestion {
+    pub name: String, 
+    pub qtype: QueryType, 
+}
 
+impl DnsQuestion {
+    pub fn new (name: String, qtype: QueryType) -> DnsQuestion {
+        DnsQuestion { name: name, qtype: qtype }
+    }
+
+    pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<()> {
+        buffer.read_qname(&mut self.name)?;
+        self.qtype = QueryType.from_num(buffer.read_u16()?); // qtype - Question Type
+        let _ = buffer.read_u16()?;   // class
+        Ok(())
+    } 
+}
 
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    println!("This is the main function ");
     Ok(())
 }
